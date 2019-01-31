ba63bfa44429b6d535a9a9753bd7aaec4fbdc9af
Bartek Pichalski <namedix@gmail.com>
2019-01-31 12:59:10 +0100

    first commit

---
+++ WeightApp/AppDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  AppDelegate.swift
+//  WeightApp
+//
+//  Created by Bartek Pichalski on 31/01/2019.
+//  Copyright © 2019 com.appnroll.pichalski. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        if let window = window {
+            window.rootViewController = ViewController()
+            window.makeKeyAndVisible()
+        }
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+}
+
---
+++ WeightApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeightApp
+//
+//  Created by Bartek Pichalski on 31/01/2019.
+//  Copyright © 2019 com.appnroll.pichalski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
